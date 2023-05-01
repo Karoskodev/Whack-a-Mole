3481cb0c5ada592bbbf1ceeb43a771b272b63037
Karosko dev <t.karavasilev@gmail.com>
2023-05-01 15:38:41 +0000

    Add comments in to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,43 @@
-// Wait for Dom to finish loading before runing the game
 
+//variable to keep track of the current hole where the mole is spawned
 let currentHole;
-let isRunning = false;
-let interval
 
+//variable to track whether the game is running or not
+let isRunning = false;
+
+let interval;
+
+
+// wait for Dom to finish loading before runing the game
 document.addEventListener("DOMContentLoaded", function() {
 
+    //start the game when the user clicks the button
     let button = document.getElementById("start")
-    button.addEventListener("click", runGame);
+    button.addEventListener("click", runGame); 
     
 })
 
+//spawn a mole every 800ms and prevent multiple functions from running at the same time
 function runGame () {
     if (!isRunning) {
         isRunning = true;
-        intervalId = setInterval(setMole, 800);
+        interval = setInterval(setMole, 800);
     } else {
-        clearInterval(intervalId);
-        intervalId = setInterval(setMole, 800);
+        clearInterval(interval); //
+        interval = setInterval(setMole, 800);
     }
 }
-    
+ 
+//generate a random number between 0 and 5 
 function randomNumber() {
     let num = Math.floor(Math.random()*6);
     return num.toString();
 }
 
+//Set the mole image in a random hole
 function setMole () {
 
+    //remove the mole from the previous hole
     if (currentHole) {
         currentHole.innerHTML = "";
     }
@@ -35,6 +45,7 @@ function setMole () {
     let mole = document.createElement("img");
     mole.src = "../assets/images/mole.png"
 
+    //Choose a random hole to put the mole in
     let num = randomNumber();
     currentHole = document.getElementById(num)
     currentHole.appendChild(mole);
